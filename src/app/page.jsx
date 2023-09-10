ba677a6198d9b2b4642f044bf19bf8c101245efa
sadbatya <sadbatya@mail.ru>
2023-09-10 19:52:30 +0200

    fix main

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,10 @@ import HomePage from '@/components/pages/Home/HomePage';
 
 export default async function Home() {
   return (
-    <div className='wrapper'>
       <div className='container'>
         <main>
           <HomePage />
         </main>
       </div>
-    </div>
   );
 }
