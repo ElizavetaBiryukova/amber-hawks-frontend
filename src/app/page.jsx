a8dcae703741b64b02009d31d2397bd6e12fced5
Elizaveta Biryukova <birliza@mail.ru>
2023-09-11 00:40:13 +0200

    fix: refactor

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,14 +1,15 @@
+import Navigation from '@/components/UI/navigation/Navigation';
 import HomePage from '@/components/pages/Home/HomePage';
 
 export default async function Home() {
   return (
-    <div className='wrapper'>
+    <>
+      <Navigation />
       <div className='container'>
         <main>
           <HomePage />
-
         </main>
       </div>
-    </div>
+    </>
   );
 }
